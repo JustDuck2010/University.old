3e2af6b6a651071dc75640c70bafd1c3b2436163
JustDuck2010 <yakv-2@yandex.ru>
2022-09-16 06:30:56 +0300

    .

--- ACS/2nd module (14.09.22), Числа/3.py
+++ ACS/2nd module (14.09.22), Числа/3.py
@@ -1,3 +1,6 @@
 #program was written 15.09.22 at home by Igor B.
 a =int(input())
 print(a//10%10, a%10, sep = '')
+
+'Дано целое число больше 9\
+Напечатайте последние две цифры числа'
--- ACS/2nd module (14.09.22), Числа/6.py
+++ ACS/2nd module (14.09.22), Числа/6.py
@@ -1,3 +1,6 @@
 #program was written 15.09.22 at home by Igor B.
 a= float(input())
 print(int(round(a,1)*10%10))
+
+'Дано вещественное положительное число\
+Напечатайте первую цифру справа после точки'
