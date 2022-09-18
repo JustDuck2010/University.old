e8ceb85f29d90ff77404f2fc974724e0e2e3365d
JustDuck2010 <yakv-2@yandex.ru>
2022-09-18 06:19:05 +0300

    .

--- ACS/3rd module, Строки/5.py
+++ ACS/3rd module, Строки/5.py
@@ -1,3 +1,4 @@
+#program was written 17.09.22 at home by Igor B.
 a = input()
 a = a.replace('@', '')
 print(a)
--- ACS/4th module, Ветвление/2.py
+++ ACS/4th module, Ветвление/2.py
@@ -1,3 +1,7 @@
+#program was written 17.09.22 at home by Igor B.
 a = int(input())
 b = int(input())
 print(min(a,b))
+
+'Даны два целых числа\
+Напечатайте меньшее из чисел'
