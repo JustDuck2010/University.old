68b31efdc8540ad42187625d7305be9d9ebd1371
JustDuck2010 <yakv-2@yandex.ru>
2023-01-27 02:49:32 +0200

    .

---
+++ ACS/6th module. Последовательности/6.py
@@ -0,0 +1,12 @@
+ar = []
+x = input
+a = [int(z) for z in a.split()]
+y = input
+b = [int(x) for x in y.split()]
+a.sort
+for i in a:
+    for j in b:
+        if a == b:
+            ar.append(a)
+            break
+print(*set(ar))
